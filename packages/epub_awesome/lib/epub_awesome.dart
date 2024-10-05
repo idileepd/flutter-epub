1fa312e8ef1c2e7847765c6689ffaf46d39af7c6
Dileep Nagendra <idileepd@gmail.com>
2024-10-05 23:56:20 +0530

    initial commit

---
+++ packages/epub_awesome/lib/epub_awesome.dart
@@ -0,0 +1,8 @@
+library epub_awesome;
+
+export 'package:epubx/epubx.dart';
+
+export 'src/ui/actual_chapter.dart';
+export 'src/ui/epub_awesome.dart';
+export 'src/ui/table_of_contents.dart';
+export 'src/helpers/epub_document.dart';
