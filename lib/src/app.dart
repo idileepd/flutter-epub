feef1fca1653244dc8ca64ad485b79163407018b
Dileep Nagendra <idileepd@gmail.com>
2024-10-06 21:11:30 +0530

    added asserts

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -2,69 +2,89 @@ import 'package:flutter/material.dart';
 import 'package:epub_awesome/epub_awesome.dart';
 import 'package:internet_file/internet_file.dart';
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+  Future<EpubController> _loadEpubController() async {
+    // const url = "https://www.gutenberg.org/ebooks/74515.epub3.images";
+    const url2 =
+        'https://github.com/daisy/epub-accessibility-tests/releases/download/fundamental-2.0/Fundamental-Accessibility-Tests-Basic-Functionality-v2.0.0.epub';
 
-class _MyHomePageState extends State<MyHomePage> {
-  late EpubController _epubReaderController;
+    //
 
-  @override
-  void initState() {
-    _epubReaderController = EpubController(
-      document:
-          // EpubDocument.openAsset('assets/New-Findings-on-Shirdi-Sai-Baba.epub'),
-          EpubDocument.openData(InternetFile.get(
-              'https://github.com/daisy/epub-accessibility-tests/releases/download/fundamental-2.0/Fundamental-Accessibility-Tests-Basic-Functionality-v2.0.0.epub')),
+    return EpubController(
+      // epubCfi: 'epubcfi(/6/26[id4]!/4/2/2[id4]/22)',
       // epubCfi:
       //     'epubcfi(/6/26[id4]!/4/2/2[id4]/22)', // book.epub Chapter 3 paragraph 10
       // epubCfi:
       //     'epubcfi(/6/6[chapter-2]!/4/2/1612)', // book_2.epub Chapter 16 paragraph 3
+      document: EpubDocument.openData(
+        await InternetFile.get(url2),
+      ),
     );
-    super.initState();
+    // document: EpubDocument.openAsset(
+    //     'assets/New-Findings-on-Shirdi-Sai-Baba.epub'));
   }
 
   @override
-  void dispose() {
-    _epubReaderController.dispose();
-    super.dispose();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('EPUB Reader'),
+      ),
+      body: FutureBuilder<EpubController>(
+        future: _loadEpubController(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            // Display a loading indicator while the EPUB is loading
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            // Handle any error while loading
+            return Center(child: Text('Error loading EPUB: ${snapshot.error}'));
+          } else if (snapshot.hasData) {
+            final epubController = snapshot.data!;
+
+            return Scaffold(
+              appBar: AppBar(
+                title: EpubViewActualChapter(
+                  controller: epubController,
+                  builder: (chapterValue) => Text(
+                    chapterValue?.chapter?.Title?.replaceAll('\n', '').trim() ??
+                        '',
+                    textAlign: TextAlign.start,
+                  ),
+                ),
+                actions: <Widget>[
+                  IconButton(
+                    icon: const Icon(Icons.save_alt),
+                    color: Colors.white,
+                    onPressed: () =>
+                        _showCurrentEpubCfi(context, epubController),
+                  ),
+                ],
+              ),
+              drawer: Drawer(
+                child: EpubViewTableOfContents(controller: epubController),
+              ),
+              body: EpubView(
+                builders: EpubViewBuilders<DefaultBuilderOptions>(
+                  options: const DefaultBuilderOptions(),
+                  chapterDividerBuilder: (_) => const Divider(),
+                ),
+                controller: epubController,
+              ),
+            );
+          } else {
+            return const Center(child: Text('No data available'));
+          }
+        },
+      ),
+    );
   }
 
-  @override
-  Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(
-          title: EpubViewActualChapter(
-            controller: _epubReaderController,
-            builder: (chapterValue) => Text(
-              chapterValue?.chapter?.Title?.replaceAll('\n', '').trim() ?? '',
-              textAlign: TextAlign.start,
-            ),
-          ),
-          actions: <Widget>[
-            IconButton(
-              icon: const Icon(Icons.save_alt),
-              color: Colors.white,
-              onPressed: () => _showCurrentEpubCfi(context),
-            ),
-          ],
-        ),
-        drawer: Drawer(
-          child: EpubViewTableOfContents(controller: _epubReaderController),
-        ),
-        body: EpubView(
-          builders: EpubViewBuilders<DefaultBuilderOptions>(
-            options: const DefaultBuilderOptions(),
-            chapterDividerBuilder: (_) => const Divider(),
-          ),
-          controller: _epubReaderController,
-        ),
-      );
-
-  void _showCurrentEpubCfi(context) {
-    final cfi = _epubReaderController.generateEpubCfi();
+  void _showCurrentEpubCfi(
+      BuildContext context, EpubController epubController) {
+    final cfi = epubController.generateEpubCfi();
 
     if (cfi != null) {
       ScaffoldMessenger.of(context).showSnackBar(
@@ -73,7 +93,7 @@ class _MyHomePageState extends State<MyHomePage> {
           action: SnackBarAction(
             label: 'GO',
             onPressed: () {
-              _epubReaderController.gotoEpubCfi(cfi);
+              epubController.gotoEpubCfi(cfi);
             },
           ),
         ),
